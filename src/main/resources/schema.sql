c26cd92c5b767829a618b32362e805907bab8b89
Alex Akulov <ws.akulov@gmail.com>
2023-04-15 14:42:32 +0300

    Changing config, styles improving

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
-create schema IF NOT EXISTS cv_platform collate utf8mb4_0900_ai_ci;
+create schema IF NOT EXISTS cv_platform;
 
 create table IF NOT EXISTS users
 (
     id       bigint auto_increment
         primary key,
     login    varchar(255) null,
-    name     varchar(255) null,
     password varchar(255) null,
     role     varchar(255) null,
+    name     varchar(255) null,
     surname  varchar(255) null,
     constraint UK_ow0gan20590jrb00upg3va2fn
         unique (login)
@@ -17,11 +17,11 @@ create table IF NOT EXISTS resumes
 (
     id        bigint auto_increment
         primary key,
-    common    text         null,
-    contacts  text         null,
-    education text         null,
     title     varchar(255) null,
+    common    text         null,
+    education text         null,
     work_exp  text         null,
+    contacts  text         null,
     user_id   bigint       null,
     constraint FK340nuaivxiy99hslr3sdydfvv
         foreign key (user_id) references users (id)
